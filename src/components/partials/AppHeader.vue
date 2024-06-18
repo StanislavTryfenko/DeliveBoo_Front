55f55b0cd336502fdec83d9e31880a0906ff891a
andreadebortoli2 <debortoliandrea88@gmail.com>
2024-06-18 18:13:09 +0200

    add nav bar in AppHeader

--- src/components/partials/AppHeader.vue
+++ src/components/partials/AppHeader.vue
@@ -1,6 +1,24 @@
 <script>
 export default {
 	name: "AppHeader",
+	data() {
+		return {
+			routes: [
+				{
+					route_name: 'home',
+					view_name: 'Home'
+				},
+				{
+					route_name: 'restaurants',
+					view_name: 'Restaurants'
+				},
+				{
+					route_name: 'cart',
+					view_name: 'Cart'
+				},
+			]
+		}
+	}
 };
 </script>
 
@@ -8,12 +26,14 @@ export default {
 	<div>
 		header
 		<nav>
+			<div class="logo">LOGO</div>
 			<ul>
-				<li>
-					<router-link :to="{ name: 'home' }">home</router-link>
+				<li v-for="route in routes">
+					<router-link activeClass :to="{ name: route.route_name }">{{ route.view_name }}</router-link>
 				</li>
 				<li>
-					<router-link :to="{ name: 'restaurants' }">restaurants</router-link>
+					<a href="http://127.0.0.1:8000/">your restaurant</a>
+					<!-- funziona se la porta è corretta (momentaneo) -->
 				</li>
 			</ul>
 		</nav>
