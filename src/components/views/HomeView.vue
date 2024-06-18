5b465b934bc1690f1c8e974c93bc660517c48f07
andreadebortoli2 <debortoliandrea88@gmail.com>
2024-06-18 18:18:13 +0200

    add home page minimal layout

--- src/components/views/HomeView.vue
+++ src/components/views/HomeView.vue
@@ -6,6 +6,27 @@ export default {
 
 <template>
     <h2>home</h2>
+
+    <div class="jumbotron">
+        <div class="filter">
+            <input type="checkbox" name="1" id="">
+            <input type="checkbox" name="2" id="">
+            <input type="checkbox" name="3" id="">
+        </div>
+        <div class="restaurants">
+            <div class="row">
+                <div class="col">
+                    <div class="card">restaurant card</div>
+                </div>
+                <div class="col">
+                    <div class="card">restaurant card</div>
+                </div>
+                <div class="col">
+                    <div class="card">restaurant card</div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <style></style>
