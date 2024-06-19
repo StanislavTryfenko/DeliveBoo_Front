2fe12ade3fe069043f065e096c27dd59e6721089
andreadebortoli2 <debortoliandrea88@gmail.com>
2024-06-19 09:40:54 +0200

    add axios, bootstrap and sass

--- src/main.js
+++ src/main.js
@@ -3,4 +3,8 @@ import './style.css'
 import App from './App.vue'
 import router from './router'
 
+import "bootstrap/dist/css/bootstrap.css"
+
 createApp(App).use(router).mount('#app')
+
+import "bootstrap/dist/js/bootstrap.js"
