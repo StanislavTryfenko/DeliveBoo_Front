8638cb4c9f6a8efa2136b0b546f59f7916e79af6
emamoldo <149785712+emamoldo@users.noreply.github.com>
2024-06-22 16:24:53 +0200

    Revert "Revert 15 emanuel type restaurants"

---
+++ src/components/partials/AppFooter.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+	name: "AppFooter",
+};
+</script>
+
+<template>
+	<hr>
+	<h2>footer</h2>
+</template>
+
+<style scoped>
+div {
+	margin-top: 3rem
+}
+</style>
---
+++ src/components/views/NotFound.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+    name: "NotFound",
+};
+</script>
+
+<template>
+    <div class="container py-5">
+        <div class="d-flex justify-content-center flex-column">
+            <br>
+            <h1>Spiacenti, questa pagina non esite.</h1>
+            <br>
+
+            <router-link :to="{ name: 'home' }">
+                <button class="btn btn-primary">Torna alla Pagina Iniziale</button>
+            </router-link>
+        </div>
+    </div>
+</template>
