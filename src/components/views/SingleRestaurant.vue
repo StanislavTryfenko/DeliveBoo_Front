867becf510def2786b2ef08f1b4314a7371b5e94
andreadebortoli2 <debortoliandrea88@gmail.com>
2024-06-27 12:12:31 +0200

    fix dish card and image layout

--- src/components/views/SingleRestaurant.vue
+++ src/components/views/SingleRestaurant.vue
@@ -147,8 +147,8 @@ export default {
 			<div class="col-12 px-5 py-2">
 				<h4>MENU</h4>
 			</div>
-			<div class="col-12 col-md-6" v-for="dish in restaurant.dishes">
-				<div class="card p-3 m-2">
+			<div class="col-12 col-md-6 g-4" v-for="dish in restaurant.dishes">
+				<div class="card p-3 m-2 h-100">
 					<div class="row">
 						<div class="col-6 col-md-9 px-3 py-1">
 							<h5>
@@ -201,5 +201,13 @@ export default {
 	background-color: lightgray;
 	width: 60%;
 	height: 20%;
+}
+
+.card {
+	img {
+		width: 100%;
+		aspect-ratio: 1;
+		object-fit: cover
+	}
 }
 </style>
